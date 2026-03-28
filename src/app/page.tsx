ac957d0d307f2b57233bdd69fa83619db0eb7885
Minher0 <minhero@outlook.fr>
2026-03-28 11:32:40 +0000

    Change Map 3D icon to map icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,9 @@ export default function Home() {
           >
             <a href="http://minewar.ddns.net:25624" target="_blank" rel="noopener noreferrer">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2">
-                <path d="M3 12h4l3 8l4 -16l3 8h4"></path>
+                <polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon>
+                <line x1="8" y1="2" x2="8" y2="18"></line>
+                <line x1="16" y1="6" x2="16" y2="22"></line>
               </svg>
               Map 3D
             </a>
